36be7ee9e5524a7d2d6a7b8ff1e19f455c621794
anudevSaraswat <mwanudev@gmail.com>
2024-05-23 14:55:16 +0530

    date format in heading

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,10 +1,32 @@
 import { WeatherInfoBox } from "./WeatherInfoBox";
 import './WeatherInfo.css';
 
+const currDate = new Date();
+const currentDate = currDate.getDate();
+const weekDay = currDate.toLocaleString('en-us', {weekday:'long'})
+const currentMonth = currDate.toLocaleString('en-us', {month:'long'})
+
+const dateStr = `${weekDay}, ${currentDate}${ordinalNumberPrefix(currentDate)} ${currentMonth} ${currDate.getFullYear()}`
+
+function ordinalNumberPrefix(date) {
+    if (date > 3 && date < 21) return 'th';
+    switch (date % 10) {
+        case 1:
+            return 'st';
+        case 2:
+            return 'nd';
+        case 3:
+            return 'rd';
+        default:
+            return 'th';
+    }
+}
+
+// Thursday, 23rd May 2023
 export function WeatherInfo() {
     return (
         <div>
-            <h2>Wednesday, 16th May 2024</h2>
+            <h2>{dateStr}</h2>
             <input></input>
             <div className="flex-container">
                 <WeatherInfoBox></WeatherInfoBox>                
